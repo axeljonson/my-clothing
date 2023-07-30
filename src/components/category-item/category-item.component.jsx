8c5dfb05f2ec2137bd4769e816c8a2f2fd567096
Jon Axelson <jon@Jons-MacBook-Pro-2.local>
2023-07-29 21:09:21 -0700

    Lesson-3: added the category-item component.
    Added the category-item component.

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,22 @@
+import "./category-item.styles.scss";
+
+// the functional component
+const CategoryItem = ({ category }) => {
+  // destructure to get the elements we need from the prop
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+export default CategoryItem;
